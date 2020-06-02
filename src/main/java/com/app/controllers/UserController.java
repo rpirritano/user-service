c07cc451111f07cda7d9099125fe7e620425f968
Robert Pirritano <rpirritano@gmail.com>
2020-06-02 13:50:03 -0700

    first commit - project setup

---
+++ src/main/java/com/app/controllers/UserController.java
@@ -0,0 +1,15 @@
+package com.app.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @RequestMapping("/user")
+    public User getUser(@RequestParam(value="id", defaultValue="1") int id) {
+        return new User(id);
+    }
+
+}
